670084d4a0ac888813a169ca59d01277dbe35355
ajo.macias@yavirac.edu.ec <ajo.macias@yavirac.edu.ecuser.nameajomacias>
2021-10-03 00:21:04 -0500

    -

---
+++ administrador/index.php
@@ -0,0 +1,61 @@
+<?php
+
+if ($_POST){
+    header('Location:inicio.php');
+}
+
+
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  </head>
+  <body>
+     
+      <div class="container">
+          <div class="row">
+          <div class="col-md-4"></div>
+              <div class="col-md-4">
+                  <br>
+                  <br>
+                  <br>
+                  <br>
+                  <br>
+                  <br>
+                  <div class="card">
+                      <div class="card-header">
+                          Login
+                      </div>
+                      <div class="card-body">
+
+                          <form method="POST" action="">
+                          <div class = "form-group">
+                          <label for="exampleInputEmail1">Usuario</label>
+                          <input type="text" class="form-control" name="usuario" aria-describedby="emailHelp" placeholder="Escribe tu usuario" required>
+                          <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                          </div>
+                          <div class="form-group">
+                          <label for="exampleInputPassword1">Password</label>
+                          <input type="password" class="form-control" name="clave" placeholder="Escribe tu contraseña" required>
+                          </div>
+                          
+                          <button type="submit" class="btn btn-primary">Ingresar admin</button>
+                          </form>
+                          
+                      </div>
+                      
+                  </div>
+              </div>
+          </div>
+      </div>
+ 
+  </body>
+</html>
---
+++ productos.php
@@ -0,0 +1,46 @@
+<?php include("./template/cabecera.php"); ?>
+
+
+<div class="col-md-3">
+<div class="card">
+    <img class="card-img-top" src="https://png.pngtree.com/element_origin_min_pic/00/00/06/12575cb97a22f0f.jpg" alt="">
+    <div class="card-body">
+        <h4 class="card-title">Title</h4>
+        <a name="" id="" class="btn btn-primary" href="#" role="button">Ver mas</a>
+    </div>
+</div>
+</div>
+
+<div class="col-md-3">
+<div class="card">
+    <img class="card-img-top" src="https://png.pngtree.com/element_origin_min_pic/00/00/06/12575cb97a22f0f.jpg" alt="">
+    <div class="card-body">
+        <h4 class="card-title">Title</h4>
+        <a name="" id="" class="btn btn-primary" href="#" role="button">Ver mas</a>
+    </div>
+</div>
+</div>
+
+<div class="col-md-3">
+<div class="card">
+    <img class="card-img-top" src="https://png.pngtree.com/element_origin_min_pic/00/00/06/12575cb97a22f0f.jpg" alt="">
+    <div class="card-body">
+        <h4 class="card-title">Title</h4>
+        <a name="" id="" class="btn btn-primary" href="#" role="button">Ver mas</a>
+    </div>
+</div>
+</div>
+
+<div class="col-md-3">
+<div class="card">
+    <img class="card-img-top" src="https://png.pngtree.com/element_origin_min_pic/00/00/06/12575cb97a22f0f.jpg" alt="">
+    <div class="card-body">
+        <h4 class="card-title">Title</h4>
+        <a name="" id="" class="btn btn-primary" href="#" role="button">Ver mas</a>
+    </div>
+</div>
+</div>
+
+
+
+<?php include("./template/pieDePagina.php"); ?>
