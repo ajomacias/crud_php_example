1e4648fa58638ebf16707d56a78d842adb9e7e41
ajo.macias@yavirac.edu.ec <ajo.macias@yavirac.edu.ecuser.nameajomacias>
2021-10-06 10:51:41 -0500

    .

--- productos.php
+++ productos.php
@@ -1,45 +1,22 @@
-<?php include("./template/cabecera.php"); ?>
+<?php include("./template/cabecera.php");
+ include("./administrador/config/db.php"); 
 
-
+$sentenciaSQL= $conexion->prepare("SELECT * FROM libros");
+$sentenciaSQL->execute();
+$listaLibros = $sentenciaSQL->fetchAll(PDO::FETCH_ASSOC);
+?>
+<?php foreach ($listaLibros as $libro){?>
 <div class="col-md-3">
 <div class="card">
-    <img class="card-img-top" src="https://png.pngtree.com/element_origin_min_pic/00/00/06/12575cb97a22f0f.jpg" alt="">
+    <img class="card-img-top" src="<?php echo $libro['imagen'] ?>" alt="">
     <div class="card-body">
-        <h4 class="card-title">Title</h4>
+        <h4 class="card-title"><?php echo $libro['nombre'] ?></h4>
         <a name="" id="" class="btn btn-primary" href="#" role="button">Ver mas</a>
     </div>
 </div>
 </div>
+<?php } ?>
 
-<div class="col-md-3">
-<div class="card">
-    <img class="card-img-top" src="https://png.pngtree.com/element_origin_min_pic/00/00/06/12575cb97a22f0f.jpg" alt="">
-    <div class="card-body">
-        <h4 class="card-title">Title</h4>
-        <a name="" id="" class="btn btn-primary" href="#" role="button">Ver mas</a>
-    </div>
-</div>
-</div>
-
-<div class="col-md-3">
-<div class="card">
-    <img class="card-img-top" src="https://png.pngtree.com/element_origin_min_pic/00/00/06/12575cb97a22f0f.jpg" alt="">
-    <div class="card-body">
-        <h4 class="card-title">Title</h4>
-        <a name="" id="" class="btn btn-primary" href="#" role="button">Ver mas</a>
-    </div>
-</div>
-</div>
-
-<div class="col-md-3">
-<div class="card">
-    <img class="card-img-top" src="https://png.pngtree.com/element_origin_min_pic/00/00/06/12575cb97a22f0f.jpg" alt="">
-    <div class="card-body">
-        <h4 class="card-title">Title</h4>
-        <a name="" id="" class="btn btn-primary" href="#" role="button">Ver mas</a>
-    </div>
-</div>
-</div>
 
 
 
