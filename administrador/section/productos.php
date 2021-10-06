c56c4a1624ad2283596c6c0a1f3ed0d49b7e1090
ajo.macias@yavirac.edu.ec <ajo.macias@yavirac.edu.ecuser.nameajomacias>
2021-10-06 10:50:41 -0500

    .

--- administrador/section/productos.php
+++ administrador/section/productos.php
@@ -14,11 +14,38 @@ switch ($accion){
         $sentenciaSQL->execute();
         break;
     case "Cancelar":
+        header("Location:productos.php");
         break;
     
     case "Modificar":
+        $sentenciaSQL= $conexion->prepare(" UPDATE libros SET nombre =:nombre, imagen = :imagen WHERE libros.id =:id;");
+        $sentenciaSQL->bindParam(':nombre',$txtNombre);
+        $sentenciaSQL->bindParam(':imagen',$txtImagen);
+        $sentenciaSQL->bindParam(':id',$txtID);
+        $sentenciaSQL->execute();
+        header("Location:productos.php");
         break;
-    }
+  
+    case "Seleccionar":
+        $sentenciaSQL= $conexion->prepare("SELECT * FROM libros WHERE libros.id=:id ");
+        $sentenciaSQL->bindParam(':id',$txtID);
+        $sentenciaSQL->execute();
+        $libro = $sentenciaSQL->fetch(PDO::FETCH_LAZY);
+
+        $txtImagen = $libro['imagen'] ;
+        $txtNombre = $libro['nombre'] ;
+        
+        
+
+        break;
+    
+    case "Borrar":
+        $sentenciaSQL= $conexion->prepare("DELETE FROM libros WHERE libros.id=:id");
+        $sentenciaSQL->bindParam(':id',$txtID);
+        $sentenciaSQL->execute();   
+        break;
+    
+}
 $sentenciaSQL= $conexion->prepare("SELECT * FROM libros");
 $sentenciaSQL->execute();
 $listaLibros = $sentenciaSQL->fetchAll(PDO::FETCH_ASSOC);
@@ -36,20 +63,20 @@ $listaLibros = $sentenciaSQL->fetchAll(PDO::FETCH_ASSOC);
         <form method="post" enctype="multipart/form-data">
 <div class = "form-group">
 <label for="txtID">ID:</label>
-<input type="text" class="form-control" name="txtID" id="txtID" aria-describedby="emailHelp" placeholder="Ingrese el id">
+<input required readonly type="text" value="<?php echo $txtID;?>"class="form-control" name="txtID" id="txtID" aria-describedby="emailHelp" placeholder="Ingrese el id">
 </div>
 <div class="form-group">
 <label for="txtNombre">Nombre de libro:</label>
-<input type="text" class="form-control" id="txtNombre" name="txtNombre" placeholder="Password">
+<input required type="text" value="<?php echo $txtNombre; ?>"class="form-control" id="txtNombre" name="txtNombre" placeholder="Password">
 </div>
 <div class="form-group">
 <label for="txtImageb">Imagen de libro:</label>
-<input type="text" class="form-control" id="txtImagen" name="txtImagen" placeholder="Ingrese la imagen">
+<input required type="text" value="<?php echo $txtImagen?>" class="form-control" id="txtImagen" name="txtImagen" placeholder="Ingrese la imagen">
 </div>
 <div class="btn-group" role="group" aria-label=""> 
-<button type="buttom" name="action" value="Agregar"  class="btn btn-success">Agregar</button>
-<button type="buttom" name="action" value="Modificar" class="btn btn-warning">Modificar</button>
-<button type="buttom" name="action" value="Cancelar" class="btn btn-info">Cancelar</button>
+<button type="buttom" <?php echo ($accion=="Seleccionar")?"disabled":""; ?> name="action" value="Agregar"  class="btn btn-success">Agregar</button>
+<button type="buttom" name="action"  <?php echo ($accion!="Seleccionar")?"disabled":""; ?>  value="Modificar" class="btn btn-warning">Modificar</button>
+<button type="buttom" name="action"  <?php echo ($accion!="Seleccionar")?"disabled":""; ?> value="Cancelar" class="btn btn-info">Cancelar</button>
 </div>
 </form>
           
@@ -75,7 +102,14 @@ $listaLibros = $sentenciaSQL->fetchAll(PDO::FETCH_ASSOC);
                 <td> <?php echo $libro['id'] ?> </td>
                 <td><?php echo $libro['nombre'] ?></td>
                 <td> <img class="img-table" src="<?php echo $libro['imagen'] ?>" alt="hola"></td>
-                <td><?php echo $libro['id'] ?></td>
+                <td>
+                    <form method="post">
+                        <input type="hidden" name="txtID" id="txtID" value="<?php echo $libro['id'] ?>"/>
+                        <input type="submit" value="Borrar" name="action" class="btn btn-danger">
+                        <input type="submit" value="Seleccionar" name="action" class="btn btn-primary"/>
+                        
+                    </form>
+                </td>
             </tr>
             <?php } ?>
         </tbody>
